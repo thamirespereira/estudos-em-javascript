ad2f0c920863c3833fd4caf32627e0e0f945566f
thamirespereira <thamiresfrpereira@hotmail.com>
2023-06-28 21:04:35 -0300

    variáveis

---
+++ variaveis.js
@@ -0,0 +1,18 @@
+var x = 26;
+var u;
+u = uva;
+//variáveis declaradas com var podem ser utilizadas antes de serem declaradas (hoisting)
+//uma variável acessada antes de ser declarada sempre será undefined
+
+let y = 99;
+let t;
+t = 6;
+//let é uma variável de escopo de bloco
+//não pode ser utilizada antes de ser declarada
+
+const z = 8;
+//não pode ser modificada
+
+
+//variável global fica disponível em qualquer parte do código
+//variáveis locais são declaradas dentro de uma função
